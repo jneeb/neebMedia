dc2e7b0dd148b25ece6149f413d4ddfd2785bc79
Justin Neeb <neebjustin@gmail.com>
2017-08-08 16:42:28 +0000

    init commit

--- site/functions/burger.js
+++ site/functions/burger.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
     $("#burger").click(function () {
         if($("#burger").hasClass("isOpen")) {
             $(this).removeClass("isOpen");
+            $("#mobileSlider").removeClass("isOpen");
         } else {
             $(this).addClass("isOpen");
+            $("#mobileSlider").addClass("isOpen");
         }
     });
 });
