82d6d280f425da1aad63328c752b4413d22b42c0
Justin Neeb <neebjustin@gmail.com>
2017-08-24 18:11:06 +0000

    changed form failure layout and form complition

--- site/seo/index.php
+++ site/seo/index.php
@@ -31,12 +31,86 @@ function check_input($data, $problem='')
 function show_error($myError)
 {
 ?>
-	<html>
-	<body>
-	<b>Please correct the following test error:</b><br />
-	<?php echo $myError; ?>
-	</body>
-	</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Toronto Web Design | NeebMedia</title>
+        <link rel="stylesheet" href="/css/main.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="icon" href="/img/favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <section id="header">
+            <div class="mobileNav">
+                <div class="mobileLogo">
+                    <a href="/"><img src="/img/neebMediaLogoWeb_noBack_black.png" alt="neebMedia"></a>
+                </div>
+
+                <div id="burger">
+                    <span></span>
+                </div>
+
+                <div id="mobileSlider">
+                    <ul>
+                        <li><a href="/portfolio">PORTFOLIO</a></li>
+                        <li><a href="#contact">CONTACT US</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="desktopNav">
+                <div class="desktopLogo"><a href="/"><img src="/img/neebMediaLogoWeb_noBack_black.png" alt="neebMedia"></a></div>
+                <ul>
+                    <li><a href="/portfolio">PORTFOLIO</a></li>
+                    <li><a class="highlight" href="#contact">CONTACT US</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <section id="portfolioHero">
+            <h1>CONTACT</h1>
+        </section>
+
+        <section id="contact">
+            <h2>INTERESTED IN WORKING WITH US?</h2>
+            <p>Call us today at <b>(647) 550 4905</b> for a free quote, or email us below.</p>
+            <p class="error">Please correct the following:</p><br />
+            <?php echo $myError; ?>
+            <section id="contactForm">
+                <form action="index.php" id="form" method="post">
+                            <input type="hidden" name="token" value="<?php echo $newToken; ?>">
+                            <input type="text" name="name" placeholder="Name:">
+                            <input type="text" name="email" placeholder="Email:">
+                            <textarea name="comments" placeholder="Tell us about the project:" id="" cols="30" rows="10"></textarea>
+                            <button type="submit" name="submit">Send</button>
+                        </form>
+            </section>
+        </section>
+
+        <section id="footer">
+            <div class="webMap">
+                <ul>
+                    <li><a href="/">HOME</a></li>
+                    <li><a href="/portfolio" class="subPage">PORTFOLIO</a></li>
+                    <li><a href="/ecommerce" class="subPage">E-COMMERCE</a></li>
+                    <li><a href="/seo" class="subPage">SEAR CH ENGINE OPTIMIZATION</a></li>
+                </ul>
+            </div>
+            <div class="socialLinks">
+                <!-- <i class="fa fa-facebook" aria-hidden="true"></i> -->
+                <a href="https://twitter.com/neebMedia"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/neebmedia/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <!-- <i class="fa fa-snapchat-ghost" aria-hidden="true"></i> -->
+                <!-- <i class="fa fa-linkedin" aria-hidden="true"></i> -->
+                <!-- <i class="fa fa-youtube-play" aria-hidden="true"></i> -->
+            </div>
+        </section>
+
+    </body>
+</html>
 <?php
 exit();
 }
@@ -45,7 +119,7 @@ if (verifyFormToken('form1')) {
 	$name = check_input($_POST['name'], "Enter your name.");
 	$email = check_input($_POST['email']);
 	$comments = check_input($_POST['comments'], "Please include a message.");
-	$subject = "Freelance Work";
+	$subject = "neebMedia - Websites";
 	if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
 	{
 	    show_error("E-mail address not valid");
@@ -74,7 +148,7 @@ if (verifyFormToken('form1')) {
 	$newToken = generateFormToken('form1');
 	?>
     <body>
-        <section id="header">
+		<section id="header">
             <div class="mobileNav">
                 <div class="mobileLogo">
                     <a href="/"><img src="/img/neebMediaLogoWeb_noBack_black.png" alt="neebMedia"></a>
@@ -96,7 +170,7 @@ if (verifyFormToken('form1')) {
                 <div class="desktopLogo"><a href="/"><img src="/img/neebMediaLogoWeb_noBack_black.png" alt="neebMedia"></a></div>
                 <ul>
                     <li><a href="/portfolio">PORTFOLIO</a></li>
-                    <!-- <li><a class="highlight" href="#contact">CONTACT US</a></li> -->
+                    <li><a class="highlight" href="#contact">CONTACT US</a></li>
                 </ul>
             </div>
         </section>
